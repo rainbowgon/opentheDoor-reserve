de5a5547b1695be35ca53505f9a622aa09759d30
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-15 04:56:56 +0900

    [fix] cloudwatch 로그 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ubuntu/crawling"
+ROOT_PATH="/home/ubuntu/app"
 
 CONTAINER="crawling_container"
 IMAGE="crawling_image"
 
+REGION="ap-northeast-2"
+GROUP="ssafy-openthedoor-log-group"
+STREAM="reserving-log-stream"
+
 docker build -t "$IMAGE" "$ROOT_PATH"
-docker run -dp 3000:3000 --name "$CONTAINER" "$IMAGE"
+docker run \
+    --log-driver=awslogs \
+    --log-opt awslogs-region="$REGION" \
+    --log-opt awslogs-group="$GROUP" \
+    --log-opt awslogs-stream="$STREAM" \
+    -dp 3000:3000 --name "$CONTAINER" "$IMAGE"
